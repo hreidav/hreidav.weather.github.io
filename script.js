ebf2edac03459a383af439090ac19a15f2f7e8e5
hreidav <v.greyda@gmail.com>
2016-06-12 13:17:33 +0300

    tenth commit

--- script.js
+++ script.js
@@ -15,12 +15,11 @@ $(function() {
 $(function() {
 
 	var APPID = "0c3ed5d45f3e7a0fec078a1a5f4e45ef";
-	
+			/*Обробка події натиснення на кнопку*/
 	$('#send').click(function() {
 		var cityname = $('#cityname').val();
 
 		/*на поточну добу*/
-
 		$.get('http://api.openweathermap.org/data/2.5/weather?q=' + cityname + '&APPID=' + APPID, function(data) {
 			$('#selectedcity').html(cityname);
 			/*відображення дня тижня на закладці поточного дня*/
@@ -30,25 +29,25 @@ $(function() {
                d=new (Date);
                switch (d.getDay()) {
                  case 0:
-                  day = "Неділя";
+                  day = "НEДIЛЯ";
                   break;
                  case 1:
-                  day = "Понеділок";
+                  day = "ПОНЕДIЛОК";
                   break;
                  case 2:
-                  day = "Вівторок";
+                  day = "ВIВТОРОК";
                   break;
                  case 3:
-                  day = "Середа";
+                  day = "СЕРЕДА";
                   break;
                  case 4:
-                  day = "Четвер";
+                  day = "ЧЕТВЕР";
                   break;
                  case 5:
-                  day = "П'ятниця";
+                  day = "П'ЯТНИЦЯ";
                   break;
                  case 6:
-                  day = "Субота";
+                  day = "СУБОТА";
                }
                return (day);
             }); 
@@ -57,17 +56,17 @@ $(function() {
 			/*хмарність іконка*/
 			$('#cloudimg').attr('src', 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png');
 			/*температура*/
-			$('#temp').html(Math.floor((data.main.temp - 273.15).toFixed(2)));
+			$('#temp').html(Math.floor((data.main.temp - 273.15).toFixed(2))+" ℃");
 			/*тиск*/
-			$('#pressure').html(Math.floor(data.main.pressure));
+			$('#pressure').html(Math.floor(data.main.pressure)+" мм.рт.ст.");
 			/*вологість*/
-			$('#humidity').html(Math.floor(data.main.humidity));
+			$('#humidity').html(Math.floor(data.main.humidity)+" %");
 			/*швидкість вітру*/
-			$('#windspeed').html(Math.floor(data.wind.speed));
+			$('#windspeed').html(Math.floor(data.wind.speed)+" м/с");
 			/*схід сонця*/
-			$('#sunrise').html(data.sys.sunrise);
+			/*$('#sunrise').html((data.sys.sunrise)+" (год:хв)");
 			/*захід сонця*/
-			$('#sunset').html(data.sys.sunset);
+			/*$('#sunset').html((data.sys.sunset) +" (год:хв)");*/
 		}, 'json').done(function() {
 			console.log('Request completed successfully');
 		}).fail(function() {
@@ -80,16 +79,19 @@ $(function() {
 /*прогноз на 5 діб*/
 		$.get('http://api.openweathermap.org/data/2.5/forecast?q=' + cityname + '&APPID=' + APPID, function(data) {
 
+/*вибірка даних на 5 діб*/
 			function weathval(index) {
-				return '<p id="clouddescr">' + data.list[index].weather[0].description + '</p><img id="cloudimg" src="http://openweathermap.org/img/w/' + data.list[index].weather[0].icon + '.png"><p id="temp">' + (data.list[index].main.temp - 273.15).toFixed(2) + '</p><p id="pressure">' + data.list[index].main.pressure + '</p><p id="humidity">' + data.list[index].main.humidity + '</p><p id="windspeed">' + data.list[index].wind.speed + '</p>'
+				return '<p id="clouddescr">' + data.list[index].weather[0].description + '</p><img id="cloudimg" src="http://openweathermap.org/img/w/' + data.list[index].weather[0].icon + '.png"><p id="temp">' + Math.floor((data.list[index].main.temp - 273.15).toFixed(2)) + '</p><p id="pressure">' + Math.floor(data.list[index].main.pressure) + '</p><p id="humidity">' + Math.floor(data.list[index].main.humidity) + '</p><p id="windspeed">' + Math.floor(data.list[index].wind.speed) + '</p>'
 			}
 			
 			$('#tab-2').append('<div id="weathervals" style="width:' + (screen.width-320) + 'px"></div>');
 
+/*динамічне створення нових елементів*/
 			for (var i = 0; i < data.list.length; i++) {
 
 				if (i == 0) {
-					$('#weathervals').append('<div id="date">' + data.list[i].dt_txt.substring(0, 10) + '<br></div>');
+					/*день тижня + дата*/    
+					$('#weathervals').append('<div id="date">' + /*(new Date(data.list[i].dt_txt).getDay()*/ data.list[i].dt_txt.substring(0, 10) + '<br></div>');
 					$('#date:last-child').append('<div class="time" id="time-' + i + '" style="border-top: 1.5px solid blue">' + data.list[i].dt_txt.substring(10, 16) + '</div>');
 					$('#time-' + i).append('<div id="weathval"></div>').append(weathval(i));
 					continue;
@@ -118,7 +120,3 @@ $(function() {
 	});
 });
 
-/*function() {
-	var ;
-return
-}*/
